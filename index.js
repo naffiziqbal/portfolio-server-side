3506becb5abd8e5cdc55f85e3049d9f77807d877
naffiziqbal <f.nafiziqbal@gmail.com>
2023-01-27 11:06:03 +0600

    Dynamic post query

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -16,18 +16,28 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
-
 async function run() {
   try {
     const postCollection = client.db("portfolio").collection("posts");
+    const userCollection = client.db("portfolio").collection('user')
 
     app.get("/posts", async (req, res) => {
       const query = {};
       const cursor = await postCollection.find(query).toArray();
       res.send(cursor);
     });
+    app.get('/post/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id : ObjectId(id)};
+      const result = await postCollection.findOne(query);
+      res.send(result)
+    })
 
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const user = await userCollection.findOne(query);
+      res.send(user)
+    });
     app.post("/post", async (req, res) => {
       const postData = req.body;
       const result = await postCollection.insertOne(postData);
@@ -45,3 +55,5 @@ app.get("/", (req, res) => {
 app.listen(Port, () => {
   console.log("Your Server Is Running");
 });
+
+module.exports = app;
