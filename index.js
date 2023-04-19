690cf0d58f4113d5395278287d8d8c44743b6dd3
naffiziqbal <f.nafiziqbal@gmail.com>
2023-04-20 02:13:12 +0600

    Making Add Portfolio Routes

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
   try {
     const postCollection = client.db("portfolio").collection("posts");
     const userCollection = client.db("portfolio").collection("user");
+    const worksCollection = client.db("portfolio").collection("works");
 
     app.get("/posts", async (req, res) => {
       const query = {};
@@ -38,6 +39,25 @@ async function run() {
       const user = await userCollection.findOne(query);
       res.send(user);
     });
+
+    app.get("/works", async (req, res) => {
+      const query = {};
+      const cursor = await worksCollection.find(query).toArray();
+      res.send(cursor);
+    });
+
+    app.get("/works/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const works = await worksCollection.findOne(query);
+      res.send(works);
+    });
+    app.post("/add-portfolio", async (req, res) => {
+      const workData = req.body;
+      const result = await worksCollection.insertOne(workData);
+      res.send(result);
+    });
+
     app.post("/post", async (req, res) => {
       const postData = req.body;
       const result = await postCollection.insertOne(postData);
